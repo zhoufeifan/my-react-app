ca8e0173be826208ec55e7f8838e3563c55e5034
ziyu <ziyu@caibaopay.com>
2018-05-26 17:58:29 +0800

    ..

--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -2,12 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.scss';
 import {Input,Button} from 'antd';
-import App from 'components/App';
+// const App = require('terrible-components');
+import {Form,SelectRow,InputRow} from 'terrible-components';
+// import App1 from 'components/App';
+// console.log(App1);
 // import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<div>
+    <Form>
+        <SelectRow
+         name="nima"
+         title="尼玛"
+         options={[{value:"1",label:'hh'},{value:"2",label:'ooo'}]}
+        ></SelectRow>
+    </Form>
     <Input/>
     <Button type="primary">尼玛</Button>
-    <App />
+    {/* <App/> */}
 </div>, document.getElementById('root'));
 // registerServiceWorker();
